4bd9676dbcb4b388101e6d47d5a69660514b130d
Алексей Селезнёв <salex_n@inbox.ru>
2022-04-02 15:33:47 +0300

    views + permissions

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets
-from api.serializers import (CategorySerializer, GenreSerializer,
-                             TitlePOSTSerializer, TitleSerializer)
 from rest_framework import mixins, viewsets
+from rest_framework.generics import get_object_or_404
 
-from reviews.models import Category, Genre, Title
+from api.serializers import (CategorySerializer, GenreSerializer,
+                             TitlePOSTSerializer, TitleSerializer,
+                             CommentSerializer, ReviewSerializer)
+from api.permissions import ReviewCommentPermission
+from reviews.models import Category, Genre, Title, Review
 
 
 class GetPostDelViewSet(mixins.CreateModelMixin,
@@ -40,8 +42,30 @@ class GenreViewSet(GetPostDelViewSet):
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = ReviewSerializer
+    permission_classes = (ReviewCommentPermission,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = CommentSerializer
+    permission_classes = (ReviewCommentPermission,)
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
