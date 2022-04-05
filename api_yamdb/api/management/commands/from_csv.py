d6ce15a78586f843ae6484c954a5a4148c539f44
Алексей Селезнёв <salex_n@inbox.ru>
2022-04-05 20:27:34 +0300

    merge2

--- api_yamdb/api/management/commands/from_csv.py
+++ api_yamdb/api/management/commands/from_csv.py
@@ -14,8 +14,13 @@ CONFORMITY = [
     {"genre.csv": "reviews_genre"},
     {"titles.csv": "reviews_title"},
     {"genre_title.csv": "reviews_title_genre"},
+<<<<<<< HEAD
     {'review.csv': 'reviews_review'},
     {'comments.csv': 'reviews_comment'},
+=======
+    {"review.csv": "reviews_review"},
+    {"comments.csv": "reviews_comment"},
+>>>>>>> master
 ]
 
 
