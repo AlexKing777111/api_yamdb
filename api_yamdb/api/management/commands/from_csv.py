58895d4d08a6a754c51690160dd829b507987755
Alex Korolev <work.alexkorolev@yandex.ru>
2022-04-08 00:09:12 +0500

    PEP8

--- api_yamdb/api/management/commands/from_csv.py
+++ api_yamdb/api/management/commands/from_csv.py
@@ -1,16 +1,7 @@
 import csv
-<<<<<<< HEAD
-import os
-import sqlite3
-
-from api_yamdb.settings import BASE_DIR
-from django.core.management.base import BaseCommand
-=======
 
 from django.conf import settings
 from django.core.management import BaseCommand
->>>>>>> c560eab5adeb52adbfcdd5c48c38548985576741
-
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 TABLES = {
@@ -24,18 +15,18 @@ TABLES = {
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **kwargs):
         for model, csv_f in TABLES.items():
             with open(
                 f"{settings.BASE_DIR}/static/data/{csv_f}",
                 "r",
-                encoding="utf-8"
+                encoding="utf-8",
             ) as csv_file:
                 reader = csv.DictReader(csv_file)
-                model.objects.bulk_create(
-                    model(**data) for data in reader)
+                model.objects.bulk_create(model(**data) for data in reader)
             print(f"  Importing data from file {csv_f}... OK")
         print()
         print("======================================")
-        self.stdout.write(self.style.SUCCESS("The all data from .csv-files are imported."))
+        self.stdout.write(
+            self.style.SUCCESS("The all data from .csv-files are imported.")
+        )
