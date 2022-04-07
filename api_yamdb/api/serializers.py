cba90e75f13c6109d884951feb9e33ec0d7fca4c
Julia Yadova <yadova.julia@yandex.ru>
2022-04-07 20:05:39 +0300

    fix all tests

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,7 +4,6 @@ from django.db.models import Avg
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from reviews.models import Category, Comment, Genre, Review, Title
-from rest_framework.validators import UniqueTogetherValidator
 
 
 class CategorySerializer(serializers.ModelSerializer):
